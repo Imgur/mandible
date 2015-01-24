dc608d06edbf3efa50f189b0543d2886588ba4ac
briankassouf <brian@imgur.com>
2015-01-24 21:45:21 +0000

    test

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("Starting server!")
 
 	// Change path to location of ImgurGo config
-	configFile := "/etc/ImgurGo/config.json"
+	configFile := "config/default.config.json"
 	if _, err := os.Stat(configFile); err != nil {
 		fmt.Printf("Configuration file %s does not exist", configFile)
 		os.Exit(-1)
