9c3bdc023fbd82d2176ce152ff5e3a6f832bb224
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-01-24 22:09:32 +0000

    Refactor switch statement for upload file type

--- view.go
+++ view.go
@@ -5,28 +5,18 @@ import (
 	"net/http"
 )
 
-// Builds and writes a successful response
-func Response(w http.ResponseWriter, data map[string]interface{}, status ...int) {
-	var responseStatus int
-	if len(status) > 0 {
-		responseStatus = status[0]
-	} else {
-		responseStatus = http.StatusOK
-	}
-
-	w.WriteHeader(responseStatus)
+// Builds and writes an error response
+func ErrorResponse(w http.ResponseWriter, message string, status int) {
+	w.WriteHeader(status)
 
 	resp, _ := json.Marshal(
 		map[string]interface{}{
-			"success": responseStatus == http.StatusOK,
-			"status":  responseStatus,
-			"data":    data,
+			"success": false,
+			"status":  status,
+			"data": map[string]string{
+				"error": message,
+			},
 		})
 
 	w.Write(resp)
-}
-
-// Builds and writes an error response
-func ErrorResponse(w http.ResponseWriter, message string, status int) {
-	Response(w, map[string]interface{}{"error": message}, status)
 }
