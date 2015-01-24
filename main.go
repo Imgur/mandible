8b42f6667ced96cd4ae84c13e82c891455c891ee
briankassouf <brian@imgur.com>
2015-01-24 21:58:22 +0000

    refactor to use an environment var for the config

--- main.go
+++ main.go
@@ -2,21 +2,18 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 )
 
 func main() {
-	// Change path to location of ImgurGo config
-	configFile := "config/default.conf.json"
-    //if _, err := os.Stat(configFile); err != nil {
-	//	fmt.Printf("Configuration file %s does not exist", configFile)
-	//	os.Exit(-1)
-	//}
-    fmt.Println("got here")
-	config := NewConfiguration(configFile)
-    fmt.Println("got here2")
+	configFile := os.Getenv("CONFIG")
+
+    if _, err := os.Stat(configFile); err != nil {
+		fmt.Printf("Configuration file %s does not exist", configFile)
+		os.Exit(-1)
+	}
+	
+    config := NewConfiguration(configFile)
 	server := CreateServer(config)
-    fmt.Println("got here3")
 	server.initServer()
-    fmt.Println("got here4")
 }
