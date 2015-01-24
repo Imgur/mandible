2a7419472aaf7533415ed89fa5086f99ac365b69
briankassouf <brian@imgur.com>
2015-01-24 22:03:42 +0000

    comment out crashing part

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"os"
 )
 
 func main() {
     configFile := os.Getenv("IMGUR_GO_CONF")
 
-    if _, err := os.Stat(configFile); err != nil {
-		fmt.Printf("Configuration file %s does not exist", configFile)
-		os.Exit(-1)
-	}
+    //if _, err := os.Stat(configFile); err != nil {
+	//	fmt.Printf("Configuration file %s does not exist", configFile)
+	//	os.Exit(-1)
+	//}
 	
     config := NewConfiguration(configFile)
 	server := CreateServer(config)
