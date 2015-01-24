29e4b99f57b6e78a7f17ca7017151d94310b18e0
briankassouf <brian@imgur.com>
2015-01-24 21:48:18 +0000

    testing a crash

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 func main() {
 	// Change path to location of ImgurGo config
 	configFile := "config/default.config.json"
-	if _, err := os.Stat(configFile); err != nil {
-		fmt.Printf("Configuration file %s does not exist", configFile)
-		os.Exit(-1)
-	}
-
+    //if _, err := os.Stat(configFile); err != nil {
+	//	fmt.Printf("Configuration file %s does not exist", configFile)
+	//	os.Exit(-1)
+	//}
+    fmt.Printf("got here")
 	config := NewConfiguration(configFile)
+    fmt.Printf("got here2")
 	server := CreateServer(config)
+    fmt.Printf("got here3")
 	server.initServer()
+    fmt.Printf("got here4")
 }
