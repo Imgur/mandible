df9a87519f7833b5aa7deb1d043a41960dc13287
Brian Kassouf <brian.m.kassouf@gmail.com>
2015-01-25 10:58:55 -0800

    add ThumbFile

---
+++ imageprocessor/thumbfile.go
@@ -0,0 +1,96 @@
+package imageprocessor
+
+import (
+    "errors"
+    //"github.com/gophergala/ImgurGo/imageprocessor/gm"
+    "github.com/gophergala/ImgurGo/uploadedfile"
+    "os"
+    "fmt"
+)
+
+type ThumbFile struct {
+    name   string
+    width  int
+    height int
+    shape  string
+    path   string
+}
+
+func (this *ThumbFile) GetName() string {
+    return this.name
+}
+
+func (this *ThumbFile) SetName(name string) {
+    this.name = name
+}
+
+func (this *ThumbFile) GetHeight() int {
+    return this.height
+}
+
+func (this *ThumbFile) SetHeight(h int) {
+    this.height = h
+}
+
+func (this *ThumbFile) GetWidth() int {
+    return this.width
+}
+
+func (this *ThumbFile) SetWidth(h int) {
+    this.width = h
+}
+
+func (this *ThumbFile) GetShape() string {
+    return this.shape
+}
+
+func (this *ThumbFile) SetShape(shape string) {
+    this.shape = shape
+}
+
+func (this *ThumbFile) GetPath() string {
+    return this.path
+}
+
+func (this *ThumbFile) SetPath(path string) error {
+    if _, err := os.Stat(path); os.IsNotExist(err) {
+        return errors.New(fmt.Sprintf("Error when creating thumbnail %s", this.GetName()))
+    }
+
+    this.path = path
+
+    return nil
+}
+
+func (this *ThumbFile) Process(original *uploadedfile.UploadedFile) error {
+    switch this.shape {
+    case "circle":
+        return this.processCircle(original)
+    case "thumb":
+        return this.processThumb(original)
+    case "square":
+        return this.processSquare(original)
+
+    }
+
+    return errors.New("Invalid thumb shape " + this.shape)
+}
+
+func (this *ThumbFile) processSquare(original *uploadedfile.UploadedFile) error {
+    return errors.New("TODO")
+    //     filename, err := gm.SquareThumb(original.GetPath(), this.GetName(), this.GetWidth())
+    // if err != nil {
+    //     return err
+    // }
+
+    // this.SetPath(filename)
+}
+
+
+func (this *ThumbFile) processCircle(original *uploadedfile.UploadedFile) error {
+    return errors.New("TODO")
+}
+
+func (this *ThumbFile) processThumb(original *uploadedfile.UploadedFile) error {
+    return errors.New("TODO")
+}
