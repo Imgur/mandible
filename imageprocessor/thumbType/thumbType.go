9346dcf0aa9e6d08fe4a35129399cfa18fc6b007
Jordan Brown <mrhmouse@gmail.com>
2015-05-04 23:27:56 -0400

    Run formatting tool

--- imageprocessor/thumbType/thumbType.go
+++ imageprocessor/thumbType/thumbType.go
@@ -1,28 +1,29 @@
 package thumbType
 
 type ThumbType int
+
 const (
-  JPG ThumbType = iota
-  PNG
-  GIF
+	JPG ThumbType = iota
+	PNG
+	GIF
 )
 
 func (this ThumbType) ToString() string {
-  if this == PNG {
-    return "PNG"
-  } else if this == GIF {
-    return "GIF"
-  } else {
-    return "JPG"
-  }
+	if this == PNG {
+		return "PNG"
+	} else if this == GIF {
+		return "GIF"
+	} else {
+		return "JPG"
+	}
 }
 
 func FromMime(mime string) ThumbType {
-  if mime == "image/gif" {
-    return GIF
-  } else if mime == "image/png" {
-    return PNG
-  } else {
-    return JPG
-  }
+	if mime == "image/gif" {
+		return GIF
+	} else if mime == "image/png" {
+		return PNG
+	} else {
+		return JPG
+	}
 }
