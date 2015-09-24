ce0eb965b834ac4aadfe848cd5bb3bd48de04b18
Cam Pedersen <cam@campedersen.com>
2015-09-24 12:13:04 -0700

    try to run goveralls in goclean

--- goclean.sh
+++ goclean.sh
@@ -35,6 +35,5 @@ done
 
 go tool cover -func profile.cov
 
-# To submit the test coverage result to coveralls.io,
-# use goveralls (https://github.com/mattn/goveralls)
-# goveralls -coverprofile=profile.cov -service=travis-ci
+env
+[ ${COVERALLS_TOKEN} ] && goveralls -coverprofile=profile.cov -service travis-ci -repotoken $COVERALLS_TOKEN"
